af8f5ccb77cee7ee17f221b8f42c1e455846ebd3
thebomuc <bothechristian8@googlemail.com>
2025-05-01 12:16:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -193,4 +193,6 @@ def index():
     return render_template("index.html", result=result + legend, filename=filename, excerpt=excerpt, highlight_line=highlight_line, suggestion="<br>".join(suggestions))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
