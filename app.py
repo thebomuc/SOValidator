b453fafae6026252718ef82789b0cc8c7ac8b07b
thebomuc <bothechristian8@googlemail.com>
2025-05-20 15:16:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -297,6 +297,7 @@ def index():
                            syntax_table=syntax_table,
                            codelist_table=codelist_table,
                            codelisten_hinweis="ℹ️ Hinweis: Codelistenprüfung basiert auf EN16931 v14 (gültig ab 2024-11-15).")
+                           original_xml=xml  # <-- nur wenn xml definiert
 
 if __name__ == "__main__":
     if hasattr(sys, '_MEIPASS'):
