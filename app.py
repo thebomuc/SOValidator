3abd1fe7aa6064e3c8e04762f5b7fdbc85a011b4
thebomuc <bothechristian8@googlemail.com>
2025-06-04 14:10:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -364,4 +364,4 @@ if __name__ == "__main__":
     if hasattr(sys, '_MEIPASS'):
         app.run(debug=True, host="127.0.0.1", port=5000)
     else:
-        app.run(debug=True, host="0.0.0.0", port=5000
+        app.run(debug=True, host="0.0.0.0", port=5000)
